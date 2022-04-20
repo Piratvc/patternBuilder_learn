9f490a6721fbaa2ca5f1f4389948017181c28f51
Piratvc <piratvc@gmail.com>
2022-04-20 20:32:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        Person mom = new PersonBuilder()
+                .setName("Анна")
+                .setSurname("Вольф")
+                .setAge(31)
+                .setAddress("Сидней")
+                .build();
+        Person son = mom.newChildBuilder()
+                .setName("Антошка")
+                .build();
+        System.out.println("У " + mom + " есть сын, " + son);
+
+        Person pers1 = new PersonBuilder()
+                .setName("Вася")
+                .setSurname("Васин")
+                .setAddress("Махачкала")
+                .build();
+
+        mom.happyBirthday();
+        System.out.println(mom);
+        System.out.println(pers1);
+
+
+
+
+        System.out.println(mom.getName());
+
+        try {
+            // Не хватает обяхательных полей
+            new PersonBuilder().build();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            // Возраст недопустимый
+            new PersonBuilder().setAge(-100).build();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+}
